371f80249ab68b2afae19e206261aecd9f7e3e3e
Maxim Odnokozov <maxvalencio@gmail.com>
2019-02-02 17:05:43 +0200

    Update JUnit tests

--- src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
+++ src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
@@ -1,38 +1,41 @@
 package net.maxvalencio.task_1_anagrams;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 
+import org.junit.Before;
+import org.junit.Test;
+import net.maxvalencio.task_1_anagrams.Anagram;
 /**
  * Unit test for AnagramImpl.
  */
 public class AnagramImplTest {
 	
+	private Anagram anagram;
+	
+	@Before 
+	public void setUp() {
+		anagram = new AnagramImpl();
+	}
+	
 	@Test
     public void When_AnagramWithoutNonLettersIsEquals_Expect_isTrue() throws Exception{
-    	AnagramImpl anagramByCollections = new AnagramImpl();
-		String s = anagramByCollections.createAnagram("abcd efgh");
-    	assertEquals("dcba hgfe", s);
+    	assertEquals("dcba hgfe",anagram.createAnagram("abcd efgh") );
     }
 	
 	@Test
     public void When_AnagramWithNonLettersIsEquals_Expect_isTrue() throws Exception{
-		AnagramImpl anagramByCollections = new AnagramImpl();
-		String s = anagramByCollections.createAnagram("a1bcd efg!h");
-    	assertEquals("d1cba hgf!e", s);
+    	assertEquals("d1cba hgf!e", anagram.createAnagram("a1bcd efg!h"));
     }
 	
 	@Test
     public void When_AnagramIsEquals_Expect_isTrue() throws Exception{
-		AnagramImpl anagramByCollections = new AnagramImpl();
-		String s = anagramByCollections.createAnagram("!!Sttt3uuu==@#ud31ennnt467 is a good programmer! Stu4&den(t 4464 =)");
-    	assertEquals("!!tnnn3edu==@#uu31utttS467 si a doog remmargorp! tne4&dut(S 4464 =)", s);
+    	assertEquals("!!tnnn3edu==@#uu31utttS467 si a doog remmargorp! tne4&dut(S 4464 =)", 
+    				anagram.createAnagram("!!Sttt3uuu==@#ud31ennnt467 "
+    									 + "is a good programmer! Stu4&den(t 4464 =)"));
     }
 	
 	@Test
     public void When_AnagramWithManySpacesIsEquals_Expect_isTrue() throws Exception{
-		AnagramImpl anagramByCollections = new AnagramImpl();
-		String s = anagramByCollections.createAnagram("  1                      1");
-    	assertEquals("1 1", s);
+    	assertEquals("1 1", anagram.createAnagram("  1                      1"));
     }	
 }
