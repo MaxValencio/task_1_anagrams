6f0ef2ffc2d102f062dcac3bd7944d181087905f
Odnokozov Maxim <maxvalencio@gmail.com>
2019-02-04 13:49:05 +0200

    Update AnagramImplTest.java

--- src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
+++ src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
@@ -11,35 +11,35 @@ public class AnagramImplTest {
 	private AnagramImpl anagram = new AnagramImpl();
 
 	@Test
-	public void createAnagram_onlyLetters_correct() throws Exception {
+	public void createAnagram_letters_correct() {
 		String actual = anagram.createAnagram("abcd efgh");
 		String expected = "dcba hgfe";
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void createAnagram_onlyNonLetters_correct() throws Exception {
+	public void createAnagram_nonLetters_correct() {
 		String actual = anagram.createAnagram("24234231");
 		String expected = "24234231";
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void createAnagram_lettersAndNonLetters_correct() throws Exception {
+	public void createAnagram_correct() {
 		String actual = anagram.createAnagram("!!tnnn3edu==@#uu31utttS467 si a doog remmargorp! tne4&dut(S 4464 =)");
 		String expected = "!!Sttt3uuu==@#ud31ennnt467 is a good programmer! Stu4&den(t 4464 =)";
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void createAnagram_spaces_correct() throws Exception {
+	public void createAnagram_spaces_correct() {
 		String actual = anagram.createAnagram("  1                      1");
 		String expected = "1 1";
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void createAnagram_inputNull_correct() throws Exception {
+	public void createAnagram_inputNull_correct() {
 		String actual = anagram.createAnagram(null);
 		String expected = "";
 		assertEquals(expected, actual);
