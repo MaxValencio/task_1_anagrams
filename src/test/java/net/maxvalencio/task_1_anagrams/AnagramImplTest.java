93d53c89c62392e7de1422520324df2d6c0c7a9b
Maxim Odnokozov <maxvalencio@gmail.com>
2019-02-04 18:37:50 +0200

    Add test createAnagram_empty_correct()

--- src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
+++ src/test/java/net/maxvalencio/task_1_anagrams/AnagramImplTest.java
@@ -11,6 +11,20 @@ public class AnagramImplTest {
 	private AnagramImpl anagram = new AnagramImpl();
 
 	@Test
+	public void createAnagram() {
+		String actual = anagram.createAnagram("!!Sttt3uuu==@#ud31ennnt467 is a good programmer! Stu4&den(t 4464 =)");
+		String expected = "!!tnnn3edu==@#uu31utttS467 si a doog remmargorp! tne4&dut(S 4464 =)";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void createAnagram_empty_correct() {
+		String actual = anagram.createAnagram("");
+		String expected = "";
+		assertEquals(expected, actual);
+	}
+
+	@Test
 	public void createAnagram_letters_correct() {
 		String actual = anagram.createAnagram("abcd efgh");
 		String expected = "dcba hgfe";
@@ -19,15 +33,8 @@ public class AnagramImplTest {
 
 	@Test
 	public void createAnagram_nonLetters_correct() {
-		String actual = anagram.createAnagram("24234231");
-		String expected = "24234231";
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void createAnagram_correct() {
-		String actual = anagram.createAnagram("!!tnnn3edu==@#uu31utttS467 si a doog remmargorp! tne4&dut(S 4464 =)");
-		String expected = "!!Sttt3uuu==@#ud31ennnt467 is a good programmer! Stu4&den(t 4464 =)";
+		String actual = anagram.createAnagram("2423423\1!!%^&#<>45?!-=_()/*6");
+		String expected = "2423423\1!!%^&#<>45?!-=_()/*6";
 		assertEquals(expected, actual);
 	}
 
